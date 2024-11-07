07046c3a41ae1319f57703b372941f54be629692
Viacheslav <slavavilhov@gmail.com>
2024-11-07 21:59:10 +0100

    fixing

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -6,38 +6,33 @@ const authenticate = async (req, res, next) => {
   try {
     const authHeader = req.get('Authorization');
 
-    // Проверка наличия заголовка Authorization
     if (!authHeader) {
       return next(createError(401, 'Authorization header is missing'));
     }
 
-    // Извлечение токена из заголовка Authorization
     const [bearer, token] = authHeader.split(' ');
+    console.log('Token:', token);
 
     if (bearer !== 'Bearer' || !token) {
       return next(createError(401, 'Auth header should be of type Bearer'));
     }
 
-    // Проверка существования сессии в базе данных
     const session = await Session.findOne({ accessToken: token });
     if (!session) {
       return next(createError(401, 'Session not found'));
     }
 
-    // Проверка срока действия токена доступа
     const isAccessTokenExpired =
       new Date() > new Date(session.accessTokenValidUntil);
     if (isAccessTokenExpired) {
       return next(createError(401, 'Access token expired'));
     }
-
-    // Проверка, что пользователь существует
     const user = await User.findById(session.userId);
     if (!user) {
       return next(createError(401, 'User not found'));
     }
 
-    req.user = user; // Добавление пользователя к запросу для дальнейшего использования
+    req.user = user;
     next();
   } catch (error) {
     next(error);
